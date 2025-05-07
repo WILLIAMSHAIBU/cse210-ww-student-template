728c7ea2f9671dffb62d106fd1db7d0024a6a1e2
WILLIAMSHAIBU <cen-01-12-20@unilia.ac.mw>
2025-05-07 01:30:42 -0700

    Update Program.cs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,39 @@
+# Author : William Shaibu 
+# Week number one, Assignment number two
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+         Console.WriteLine("Please Enter your grade: ");
+       string grade = Console.ReadLine();
+
+       int Grade = int.Parse(grade);
+
+       if((Grade>=90)&&(Grade<=100))
+       {
+        Console.WriteLine("A");
+       }
+       else if((Grade>=80)&&(Grade<90))
+       {
+        Console.WriteLine("B");
+       }
+       else if((Grade >= 70)&&(Grade < 80))
+       {
+        Console.WriteLine("C");
+       }
+       else if((Grade>= 60)&&(Grade < 70))
+       {
+        Console.WriteLine("D");
+       }
+       else if(Grade< 60)
+       {
+        Console.WriteLine("F");
+       }
+       else{
+        Console.WriteLine("Invalid Grades");
+       }
     }
 }
